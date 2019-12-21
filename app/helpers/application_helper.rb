73ecc81ea5e88b6614bf26651edb68c146593e4c
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2019-12-21 17:19:53 +0900

    関数を組み込んで|のバグを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def full_title(page_title="")
+        base_title = "文系学生と学ぶ社会学"
+        if page_title.empty?
+            base_title
+        else
+            page_title + '|' + base_title
+        end
+    end
 end
