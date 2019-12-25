8ca969c886bab977f88b7df3643cfe28a5b0645e
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2019-12-25 22:18:43 +0900

    550エラーを解消したい

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,8 @@ Rails.application.configure do
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
-    user_name: ENV['app156436276@heroku.com'],
-    password: ENV['muu5fgpd3907'],
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
