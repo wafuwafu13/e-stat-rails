db0befff4d2ddf0b84e368309f89761a85e1f415
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2019-12-25 14:53:48 +0900

    有効化のアクション実装とログインの設定

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,16 @@
 class AccountActivationsController < ApplicationController
+
+    def edit
+        user = User.find_by(email: params[:email])
+        if user && !user.activated? && user.authenticated?(:activation, params[:id])
+            user.update_attribute(:activated, true)
+            user.update_attribute(:activated_at, Time.zone.now)
+            log_in user
+            flash[:success] = "アカウントが有効になりました！"
+            redirect_to user
+        else
+            flash[:danger] = "有効化のりんくが正しくありません。"
+            redirect_to root_url
+        end
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email:params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.acrivated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = "アカウントが有効化されていません。"
+        message += "メールで有効化へのリンクを確認してください。"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'メールアドレスとパスワードが違います'
       render 'new'
