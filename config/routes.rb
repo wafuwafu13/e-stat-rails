ab402bb45bdf461ade6e145d7c597a0ed120d783
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2019-12-25 17:13:25 +0900

    Blogのルートとコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -32,4 +32,5 @@ Rails.application.routes.draw do
   delete '/logout', to:'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :blogs, only: [:create, :destroy]
 end
