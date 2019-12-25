6cf7c878fa5bee2d51c957fae54f77a0993b14c4
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2019-12-25 17:26:10 +0900

    Blogのcreateアクションを作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,8 +2,20 @@ class BlogsController < ApplicationController
     before_action :logged_in_user, only[:create, :destroy]
 
     def create
+        @blog = current_user.blogs.build(blog_params)
+        if @blog.save
+            flash[:success] = "ブログが作成されました！"
+            redirect_to root_url
+        else
+            render 'static_pages/home'
+        end
     end
 
     def destroy
     end
+
+    private
+      
+      def blog_params
+        params.require(:blog).permit(:content)
 end
