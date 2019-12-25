eb88efd0455ae066aaa18a8aa9975fa17c04aea2
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2019-12-25 15:37:21 +0900

    rails generate model Micropost content:text user:referencesの実行

---
+++ app/models/blog.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: blogs
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_blogs_on_user_id                 (user_id)
+#  index_blogs_on_user_id_and_created_at  (user_id,created_at)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+
+class Blog < ApplicationRecord
+  belongs_to :user
+end
---
+++ test/models/blog_test.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: blogs
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_blogs_on_user_id                 (user_id)
+#  index_blogs_on_user_id_and_created_at  (user_id,created_at)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+
+require 'test_helper'
+
+class BlogTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
