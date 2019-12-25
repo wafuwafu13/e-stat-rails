c6840f7c12408852c991230fc13d1431bcc8b7bb
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2019-12-25 12:59:04 +0900

    1人をadminユーザーにする

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@
 User.create!(name: "Example User",
         email: "example@i.example.jp",
         password: "hogehoge",
-        password_confirmation: "hogehoge")
+        password_confirmation: "hogehoge",
+        admin: true)
 
 99.times do |n|
     name = Faker::Name.name
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id              :integer          not null, primary key
+#  admin           :boolean          default(FALSE)
 #  email           :string
 #  name            :string
 #  password_digest :string
