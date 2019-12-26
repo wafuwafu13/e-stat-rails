fd6e563a8de7bbf485709e7dc4786f45f510fda6
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2019-12-26 09:58:31 +0900

    ブログを削除できるようにする

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,10 @@
 class BlogsController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
+    before_action :correct_user, only: :destroy
 
     def create
         @blog = current_user.blogs.build(blog_params)
+        binding.pry
         if @blog.save
             flash[:success] = "ブログが作成されました！"
             redirect_to root_url
@@ -13,6 +15,9 @@ class BlogsController < ApplicationController
     end
 
     def destroy
+        @blog.destroy
+        flash[:success] = "ブログを削除しました"
+        redirect_to request.referrer || root_url
     end
 
     private
@@ -20,4 +25,10 @@ class BlogsController < ApplicationController
       def blog_params
         params.require(:blog).permit(:content)
       end
+
+      def correct_user
+        @blog = current_user.blogs.find_by(id: params[:id])
+        redirect_to root_url if @blog.nil?
+      end
+    
 end
