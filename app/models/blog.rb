54c9d3fbda687098ad375ef0870019eef3f5d290
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2019-12-26 10:16:25 +0900

    画像投稿をできるようにする

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -22,6 +22,7 @@
 class Blog < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 1000 }
 end
