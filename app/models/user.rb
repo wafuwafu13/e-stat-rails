2e84ea92c9047ae64799978214d82e82148e5efb
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2019-12-26 15:35:31 +0900

    seedで関係を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
     has_many :blogs, dependent: :destroy
     has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
     has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-    has_many :following, through: :active_relationships, source: :follwed
+    has_many :following, through: :active_relationships, source: :followed
     has_many :followers, through: :passive_relationships, source: :follower
     attr_accessor :remember_token, :activation_token
     before_save :downcase_email
