fd473369276b5db6fdd375d43cbe084d79517ee0
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2019-12-26 15:56:42 +0900

    フォロー関係をhomeに表示

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,11 @@ Rails.application.routes.draw do
   get '/login', to:'sessions#new'
   post '/login', to:'sessions#create'
   delete '/logout', to:'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :users
   resources :account_activations, only: [:edit]
   resources :blogs, only: [:create, :destroy]
