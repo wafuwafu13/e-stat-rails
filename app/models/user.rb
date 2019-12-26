4298d82b2bed1d2cb3dc3fedac6c27d87a5b6a5b
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2019-12-26 15:11:56 +0900

    関連づけとfollowing関連のメソッドの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,7 @@
 class User < ApplicationRecord
     has_many :blogs, dependent: :destroy
     has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+    has_many :following, through: :active_relationships, source: :follwed
     attr_accessor :remember_token, :activation_token
     before_save :downcase_email
     before_create :create_activation_digest
@@ -78,6 +79,18 @@ class User < ApplicationRecord
 
     def feed
         Blog.where("user_id=?", id)
+    end
+
+    def follow(other_user)
+        following << other_user
+    end
+
+    def unfollow(other_user)
+        active_relationships.find_by(followed_id: other_user.id).destroy
+    end
+
+    def following?(other_user)
+        following.include?(other_user)
     end
 end
 
