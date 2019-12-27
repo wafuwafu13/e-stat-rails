1b8bf7cf88e88305a805afaad034db506d951928
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2019-12-27 12:54:12 +0900

    投稿パスを作るためにnewコントローラー、ルート、テンプレートを作成した

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,22 +2,25 @@ class BlogsController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
     before_action :correct_user, only: :destroy
 
+    def new
+      @blog = Blog.new()
+    end
+        
     def create
-        @blog = current_user.blogs.build(blog_params)
-        if @blog.save
-            flash[:success] = "ブログが作成されました！"
-            redirect_to root_url
-        else
-            
-            @feed_items = []
-            render 'static_pages/home'
-        end
+      @blog = current_user.blogs.build(blog_params)
+      if @blog.save
+          flash[:success] = "ブログが作成されました！"
+          redirect_to root_url
+      else
+          @feed_items = []
+          render 'static_pages/home'
+      end
     end
 
     def destroy
-        @blog.destroy
-        flash[:success] = "ブログを削除しました"
-        redirect_to request.referrer || root_url
+      @blog.destroy
+      flash[:success] = "ブログを削除しました"
+      redirect_to request.referrer || root_url
     end
 
     private
