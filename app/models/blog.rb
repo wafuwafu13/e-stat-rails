572947f1c3317fb9d78c82a5253881712668d115
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2019-12-30 13:39:50 +0900

    dependentの設定

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -22,7 +22,7 @@
 
 class Blog < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
