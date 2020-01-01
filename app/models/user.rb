5381a6af837aed1d03225193024b35c0ada55a55
wafuwafu13 <h.tagawa.miraidenshi@gmail.com>
2020-01-01 15:12:24 +0900

    メールアドレスを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,13 +19,8 @@
 
 class User < ApplicationRecord
     has_many :blogs, dependent: :destroy
-    before_save :downcase_email
     mount_uploader :image, PictureUploader
     validates :name, presence: true, length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 },
-               format: { with: VALID_EMAIL_REGEX },
-               uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
     validates :introduction, length: { maximum: 200 }
@@ -35,10 +30,6 @@ class User < ApplicationRecord
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                   BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
-    end
-
-    def downcase_email
-        self.email = email.downcase
     end
 
     private
